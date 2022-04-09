4238450b54d92d8606dd221079c112a0b930e1dd
Ggodlyy <nikogerenski@abv.bg>
2022-04-09 19:14:56 +0300

    add books component

--- O-Books/src/app/feature/books/books.module.ts
+++ O-Books/src/app/feature/books/books.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { BooksListComponent } from './books-list/books-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BooksPageComponent,
+    BooksListComponent
+  ],
   imports: [
     CommonModule
   ]
