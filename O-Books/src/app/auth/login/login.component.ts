a95bef0eb2670b51695027c20ce733378a1eaa74
Ggodlyy <nikogerenski@abv.bg>
2022-04-12 11:18:06 +0300

    added button disable unit form is correct

--- O-Books/src/app/auth/login/login.component.ts
+++ O-Books/src/app/auth/login/login.component.ts
@@ -20,6 +20,6 @@ export class LoginComponent implements OnInit {
   }
 
   loginHandler(): void {
-    console.log(this.loginFormGroup.controls['password'].errors);
+    console.log(this.loginFormGroup);
   }
 }
