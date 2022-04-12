be548bcd03c0c6117c6edbbf95baa421016ca33a
Ggodlyy <nikogerenski@abv.bg>
2022-04-12 19:52:24 +0300

    added add-book page html and css
    also added new route

--- O-Books/src/app/feature/books/books-routing.module.ts
+++ O-Books/src/app/feature/books/books-routing.module.ts
@@ -1,4 +1,5 @@
 import { RouterModule, Routes } from "@angular/router";
+import { AddBookComponent } from "./add-book/add-book.component";
 import { BooksListComponent } from "./books-list/books-list.component";
 
 
@@ -9,6 +10,10 @@ const routes: Routes = [
         path: 'library',
         component: BooksListComponent
     },
+    {
+        path: 'add-book',
+        component: AddBookComponent
+    }
    
    
 ];
