76ab20bd2d315b760df923d36ed01f2fd6661a64
Ggodlyy <nikogerenski@abv.bg>
2022-04-12 18:25:24 +0300

    add book-list-item component

--- O-Books/src/app/feature/books/books.module.ts
+++ O-Books/src/app/feature/books/books.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BookRoutingModule } from './books-routing.module';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
 
 
 
 @NgModule({
   declarations: [
-    BooksListComponent
+    BooksListComponent,
+    BookListItemComponent
   ],
   imports: [
     CommonModule,
