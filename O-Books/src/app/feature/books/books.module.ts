1d75b43f943775d421ed88e2a657c8ffeec627ce
Ggodlyy <nikogerenski@abv.bg>
2022-04-12 19:33:14 +0300

    added add-book page

--- O-Books/src/app/feature/books/books.module.ts
+++ O-Books/src/app/feature/books/books.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BookRoutingModule } from './books-routing.module';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 
 
 @NgModule({
   declarations: [
     BooksListComponent,
-    BookListItemComponent
+    BookListItemComponent,
+    AddBookComponent
   ],
   imports: [
     CommonModule,
