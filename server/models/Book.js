e5fa9ba8d43b3c4273795b4dd369d9f1abb8e6a0
Ggodlyy <nikogerenski@abv.bg>
2022-04-12 18:38:06 +0300

    improve on html structure for book-list

--- server/models/Book.js
+++ server/models/Book.js
@@ -3,9 +3,10 @@ const { model, Schema, Types: { ObjectId } } = require('mongoose');
 const schema = new Schema({
     title: { type: String, required: [true, 'Title is required'] },
     author: { type: String, required: true },
+    genre: {type: String, required: true},
+    img: { type: String, required: true },
     description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
     price: { type: Number, required: true },
-    img: { type: String, required: true },
     owner: { type: ObjectId, ref: 'User' },
     likes: { type: [ObjectId], ref: 'User' },
 });
