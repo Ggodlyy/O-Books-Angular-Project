15a973e3ed6f75e6be7567f10de168ff4d56cc45
Ggodlyy <nikogerenski@abv.bg>
2022-04-13 12:33:09 +0300

    changed a bit of book-list-item css

--- O-Books/src/app/feature/books/book-list-item/book-list-item.component.ts
+++ O-Books/src/app/feature/books/book-list-item/book-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IBook } from 'src/app/core/interfaces/book';
 
 @Component({
   selector: 'app-book-list-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list-item.component.css']
 })
 export class BookListItemComponent implements OnInit {
+
+  @Input() book: IBook;
 
   constructor() { }
 
