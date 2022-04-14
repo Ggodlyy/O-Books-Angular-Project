322f059b6ca996e3de1e55e615fd711234111e3c
Ggodlyy <nikogerenski@abv.bg>
2022-04-14 10:56:06 +0300

    adjusted some logic in myProfile
    adjusted myBookList removed unnecessary list

--- O-Books/src/app/auth/profile/profile.component.ts
+++ O-Books/src/app/auth/profile/profile.component.ts
@@ -9,9 +9,8 @@ import { UserService } from 'src/app/core/user.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit, AfterViewInit {
+export class ProfileComponent implements OnInit {
 
-  bookList: IBook[];
   myBookList: IBook[];
 
   currentUser: IUser;
@@ -24,8 +23,7 @@ export class ProfileComponent implements OnInit, AfterViewInit {
         this.currentUser = user;
 
         this.bookService.loadBookList$().subscribe(booklist => {
-          this.bookList = booklist;
-          this.myBookList = this.bookList.filter(b => b.owner === this?.currentUser._id);
+          this.myBookList = booklist.filter(b => b.owner === this?.currentUser._id);
         });
       },
       error: (err) => {
@@ -37,9 +35,6 @@ export class ProfileComponent implements OnInit, AfterViewInit {
 
   }
 
-  ngAfterViewInit(): void {
-   
-
-  }
+ 
 
 }
