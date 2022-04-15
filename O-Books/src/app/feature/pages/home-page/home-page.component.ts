872225c47a493d4b7a32cdf79f6e4f57bf3b6d21
Ggodlyy <nikogerenski@abv.bg>
2022-04-15 20:32:37 +0300

    added search functionality in home-page

--- O-Books/src/app/feature/pages/home-page/home-page.component.ts
+++ O-Books/src/app/feature/pages/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/core/book.service';
+import { IBook } from 'src/app/core/interfaces/book';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  book: IBook;
+  searchPressed: boolean;
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
   }
 
+  searchHandler(searchForm) {
+    this.searchPressed = true;
+    let searchValue = searchForm.value.search;
+
+    if (searchValue === '') {
+      this.book = undefined;
+      return;
+    }
+
+    this.bookService.loadBookList$().subscribe(bookList => {
+      let pattern = new RegExp(searchValue, 'i');
+      const myBookList = bookList.slice();
+     
+      for (let i = 0; i < myBookList.length; i++) {
+        let currentBook = myBookList[i];
+
+        if(pattern.test(currentBook.title)) {
+          this.book = currentBook;
+          break;
+        }
+      }
+    
+    })
+
+  }
 }
--- O-Books/src/app/feature/pages/pages.module.ts
+++ O-Books/src/app/feature/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { BooksModule } from '../books/books.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +13,9 @@ import { HomePageComponent } from './home-page/home-page.component';
     NotFoundPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BooksModule,
+    FormsModule,
   ]
 })
 export class PagesModule { }
